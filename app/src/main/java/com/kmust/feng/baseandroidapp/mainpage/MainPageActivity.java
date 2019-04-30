3a82e1acaed1c28c50a6f232c31b001a1dbbb6bf
lihuifeng <lihuifeng@yy.com>
2019-04-30 14:55:12 +0800

    调整页面跳转逻辑&UI

--- app/src/main/java/com/kmust/feng/baseandroidapp/mainpage/MainPageActivity.java
+++ app/src/main/java/com/kmust/feng/baseandroidapp/mainpage/MainPageActivity.java
@@ -1,6 +1,7 @@
 package com.kmust.feng.baseandroidapp.mainpage;
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 
@@ -16,9 +17,18 @@ public class MainPageActivity extends BaseActivity<MainPageViewModel> {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // 设置沉浸式状态栏
-        StatusBarUtils.setImmersiveStatusBar(this, Color.TRANSPARENT);
+        StatusBarUtils.setImmersiveStatusBar(this, Color.WHITE);
         // create viewModel
         mViewModel = ViewModelProviders.of(this).get(VIEW_MODEL_TAG, MainPageViewModel.class);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // 按返回键回到桌面
+        Intent home = new Intent(Intent.ACTION_MAIN);
+        home.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        home.addCategory(Intent.CATEGORY_HOME);
+        startActivity(home);
     }
 }
